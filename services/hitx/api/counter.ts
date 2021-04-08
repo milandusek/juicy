8ce8ac4246a01040faafc7d183c91770481edd41
milandusek <aldria@post.cz>
2021-04-08 19:14:21 +0200

    Update counter.ts

--- services/hitx/api/counter.ts
+++ services/hitx/api/counter.ts
@@ -1,8 +1,8 @@
 import { NowRequest, NowResponse } from '@vercel/node';
-import * as db from "./_lib/db";
-import { getTemplate } from './_lib/template';
+
 
 export default async function handler(req: NowRequest, res: NowResponse) {
+   console.log("HTTP", req.url);  
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.setHeader("Cache-Control", "max-age=1, s-maxage=1, stale-while-revalidate");
